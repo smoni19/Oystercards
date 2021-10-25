f26d4aaa728687e84dbdf041791bbf7a9f724d18
dean <d.lewis.rave@gmail.com>
2021-10-25 18:32:46 +0100

    Charge on touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,38 @@
 class Oystercard
-  attr_reader :balance, :limit
+  attr_reader :balance, :limit, :in_use
   DEFAULT_LIMIT = 90
+  MINIMUM_AMOUNT = 1
 
-  default_limit = DEFAULT_LIMIT
   def initialize(limit = DEFAULT_LIMIT)
     @balance = 0
     @limit = limit
+    @in_use = false
   end
 
   def top_up(amount)
     @balance + amount > @limit ? fail('Max limit of #{default_limit} GBP reached') : @balance += amount
+  end
+
+  def touch_in
+    if @balance < MINIMUM_AMOUNT
+      fail "Please top up"
+    else
+      @in_use = true
+    end
+  end
+
+  def touch_out
+    deduct(MINIMUM_AMOUNT)
+    @in_use = false
+  end
+
+  def in_journey?
+    @in_use 
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -20,4 +20,44 @@ describe Oystercard do
       subject.top_up default_limit
       expect { subject.top_up 1 }.to raise_error 'Max limit of #{default_limit} GBP reached'
     end
+
+    # commented out because its now managed by touch in
+    #
+    # describe 'deduct' do
+    #   it 'deducts money from oystercard' do
+    #     oystercard = Oystercard.new
+    #     subject.top_up(10)
+    #     expect { subject.deduct(5) }.to change { subject.balance }.by(-5)
+    #   end
+    # end
+
+    describe 'touch_in' do
+      it 'it changes in use to true' do
+        oystercard = Oystercard.new
+        subject.top_up(10)
+        expect { subject.touch_in }.to change { subject.in_use }.from(false).to(true)
+      end
+      it 'doesn/t allow user to touch in when balance is less than 1 pound' do
+        oystercard = Oystercard.new
+        expect { subject.touch_in }.to raise_error 'Please top up'
+      end
+    end
+
+    describe 'touch_out' do
+      it 'it changes in use to false' do
+        oystercard = Oystercard.new
+        subject.top_up(10)
+        subject.touch_in
+        expect { subject.touch_out }.to change { subject.in_use }.from(true).to(false)
+      end
+    end
+
+    describe 'in_journey?' do
+      it 'returns true if touched in' do
+        oystercard = Oystercard.new
+        subject.top_up(10)
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+    end
 end
