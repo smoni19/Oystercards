20608c9e72f9c0471f7b8f57a4ff01245da6448a
Simon Jacobs <simonjacobs@simons-air.home>
2021-10-27 13:14:13 +0100

    Completed 11 - saving entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,33 +1,30 @@
 class Oystercard
-  attr_reader :balance, :limit, :in_use
+  attr_reader :balance, :limit, :entry_station
+
   DEFAULT_LIMIT = 90
   MINIMUM_AMOUNT = 1
 
   def initialize(limit = DEFAULT_LIMIT)
     @balance = 0
     @limit = limit
-    @in_use = false
+    @entry_station = nil
   end
 
   def top_up(amount)
-    @balance + amount > @limit ? fail('Max limit of #{default_limit} GBP reached') : @balance += amount
+    @balance + amount > @limit ? fail("Max limit of #{DEFAULT_LIMIT} GBP reached") : @balance += amount
   end
 
-  def touch_in
-    if @balance < MINIMUM_AMOUNT
-      fail "Please top up"
-    else
-      @in_use = true
-    end
+  def touch_in(station)
+    @balance < MINIMUM_AMOUNT ? fail("Please top up") : @entry_station = station
   end
 
   def touch_out
     deduct(MINIMUM_AMOUNT)
-    @in_use = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_use 
+    !!@entry_station
   end
 
   private
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,9 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:oystercard) { Oystercard.new }
+
   it 'returns balance 0' do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq 0
+    expect(subject.balance).to eq 0
   end
     describe '#top_up' do
       it 'increases balance by amount given' do
@@ -11,14 +12,12 @@ describe Oystercard do
       end
     end
     it 'has a maximum limit of 90' do
-      oystercard = Oystercard.new
-      expect(oystercard.limit).to eq 90
+      expect(subject.limit).to eq 90
     end
 
-    it 'raises and error if balance + top_up amount is greater than limit' do
-      default_limit = Oystercard::DEFAULT_LIMIT
-      subject.top_up default_limit
-      expect { subject.top_up 1 }.to raise_error 'Max limit of #{default_limit} GBP reached'
+    it 'raises and error if balance + #top_up amount is greater than limit' do
+      subject.top_up(Oystercard::DEFAULT_LIMIT)
+      expect { subject.top_up 1 }.to raise_error "Max limit of #{Oystercard::DEFAULT_LIMIT} GBP reached"
     end
 
     # commented out because its now managed by touch in
@@ -31,32 +30,41 @@ describe Oystercard do
     #   end
     # end
 
-    describe 'touch_in' do
+    describe '#touch_in' do
+      let(:station){ double :station }
+
       it 'it changes in use to true' do
-        oystercard = Oystercard.new
         subject.top_up(10)
-        expect { subject.touch_in }.to change { subject.in_use }.from(false).to(true)
+        expect { subject.touch_in(station) }.to change { subject.entry_station }.from(nil).to(station)
       end
       it 'doesn/t allow user to touch in when balance is less than 1 pound' do
-        oystercard = Oystercard.new
-        expect { subject.touch_in }.to raise_error 'Please top up'
+        expect { subject.touch_in(station) }.to raise_error 'Please top up'
       end
-    end
-
-    describe 'touch_out' do
-      it 'it changes in use to false' do
-        oystercard = Oystercard.new
+      it 'takes a station argument and changes entry_station to it' do
         subject.top_up(10)
-        subject.touch_in
-        expect { subject.touch_out }.to change { subject.in_use }.from(true).to(false)
+        expect { subject.touch_in(station) }.to change { subject.entry_station }.to station
       end
     end
 
-    describe 'in_journey?' do
+    describe '#touch_out' do
+      let(:station){ double :station }
+      before { subject.top_up(10) }
+      before { subject.touch_in(station) }
+      
+      it 'it changes entry_station to nil' do
+        expect { subject.touch_out }.to change { subject.entry_station }.from(station).to(nil)
+      end
+      it 'deducts fare from balance' do
+        expect { subject.touch_out }.to change { subject.balance }.by(-1)
+      end
+    end
+
+    describe '#in_journey?' do
+      let(:station){ double :station }
+
       it 'returns true if touched in' do
-        oystercard = Oystercard.new
         subject.top_up(10)
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject).to be_in_journey
       end
     end
