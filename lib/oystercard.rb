9cfabdcba3468a62e01ffc40e053fe76382ec162
Simon Jacobs <simonjacobs@simons-air.home>
2021-10-28 13:26:18 +0100

    Challenge 12 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,30 +1,38 @@
 class Oystercard
-  attr_reader :balance, :limit, :entry_station
+  attr_reader :balance, :limit, :entry_station, :all_journeys
 
   DEFAULT_LIMIT = 90
-  MINIMUM_AMOUNT = 1
+  MINIMUM_FARE = 1
 
   def initialize(limit = DEFAULT_LIMIT)
     @balance = 0
     @limit = limit
     @entry_station = nil
+    @all_journeys = []
+    @single_journey = {:entry_station => "", :exit_station => ""}
   end
 
   def top_up(amount)
     @balance + amount > @limit ? fail("Max limit of #{DEFAULT_LIMIT} GBP reached") : @balance += amount
   end
 
-  def touch_in(station)
-    @balance < MINIMUM_AMOUNT ? fail("Please top up") : @entry_station = station
+  def touch_in(entry_station)
+    @balance < MINIMUM_FARE ? fail("Please top up") : @entry_station = entry_station
   end
 
-  def touch_out
-    deduct(MINIMUM_AMOUNT)
+  def touch_out(exit_station)
+    deduct(MINIMUM_FARE)
+    record_journey(@entry_station, exit_station)
     @entry_station = nil
   end
 
   def in_journey?
     !!@entry_station
+  end
+
+  def record_journey(entry_station, exit_station)
+    @single_journey = {:entry_station => entry_station, :exit_station => exit_station}
+    @all_journeys << @single_journey
   end
 
   private
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,8 +2,9 @@ require 'oystercard'
 
 describe Oystercard do
   let(:oystercard) { Oystercard.new }
+  let(:station){ double 'station' }
 
-  it 'returns balance 0' do
+  it 'initialises with a balance of 0' do
     expect(subject.balance).to eq 0
   end
     describe '#top_up' do
@@ -15,7 +16,7 @@ describe Oystercard do
       expect(subject.limit).to eq 90
     end
 
-    it 'raises and error if balance + #top_up amount is greater than limit' do
+    it 'raises an error if balance + #top_up amount is greater than limit' do
       subject.top_up(Oystercard::DEFAULT_LIMIT)
       expect { subject.top_up 1 }.to raise_error "Max limit of #{Oystercard::DEFAULT_LIMIT} GBP reached"
     end
@@ -31,13 +32,11 @@ describe Oystercard do
     # end
 
     describe '#touch_in' do
-      let(:station){ double :station }
-
-      it 'it changes in use to true' do
+      it 'changes in_use to true' do
         subject.top_up(10)
         expect { subject.touch_in(station) }.to change { subject.entry_station }.from(nil).to(station)
       end
-      it 'doesn/t allow user to touch in when balance is less than 1 pound' do
+      it 'doesn\'t allow user to touch in when balance is less than 1 pound' do
         expect { subject.touch_in(station) }.to raise_error 'Please top up'
       end
       it 'takes a station argument and changes entry_station to it' do
@@ -47,25 +46,34 @@ describe Oystercard do
     end
 
     describe '#touch_out' do
-      let(:station){ double :station }
       before { subject.top_up(10) }
       before { subject.touch_in(station) }
-      
-      it 'it changes entry_station to nil' do
-        expect { subject.touch_out }.to change { subject.entry_station }.from(station).to(nil)
+      it 'changes entry_station to nil' do
+        expect { subject.touch_out(station) }.to change { subject.entry_station }.from(station).to(nil)
       end
       it 'deducts fare from balance' do
-        expect { subject.touch_out }.to change { subject.balance }.by(-1)
+        expect { subject.touch_out(station) }.to change { subject.balance }.by(-1)
       end
     end
 
     describe '#in_journey?' do
-      let(:station){ double :station }
-
       it 'returns true if touched in' do
         subject.top_up(10)
         subject.touch_in(station)
         expect(subject).to be_in_journey
       end
+    end
+
+    it 'card has an empty list of journeys by default' do
+      expect(subject.all_journeys).to be_empty
+    end
+
+    it 'creates one journey when touching in and out' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out(station)
+      subject.touch_in(station)
+      subject.touch_out(station)
+      expect(subject.all_journeys.length).to eq 2
     end
 end
